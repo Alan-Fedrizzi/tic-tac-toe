5989c7393235d9b59781ab72fefe8bf5c3ebba76
alanfedrizzi <alanfedrizzi@gmail.com>
2023-01-14 18:15:43 -0300

    checkWinner and ShowWinner functionality

--- script.js
+++ script.js
@@ -2,9 +2,20 @@
 
 const gridCells = document.querySelectorAll(".grid__cell");
 const gridButtons = document.querySelectorAll(".grid__button");
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal__content");
 
 let player = 1;
 const gridArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+const resetGame = function () {
+  player = 1;
+  gridArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  gridButtons.forEach((button) => {
+    button.innerHTML = "";
+  });
+};
 
 const renderIcon = function (currentPlayer, btn) {
   const html = `
@@ -24,43 +35,71 @@ const changePlayer = function (currentPlayer) {
   currentPlayer === 1 ? (player = 2) : (player = 1);
 };
 
-const assingPlayer = function (btn, currentPlayer) {
+const assingPlayer = function (btn, position, currentPlayer) {
   renderIcon(currentPlayer, btn);
   disableButton(btn);
-  changePlayer(currentPlayer);
+  gridArray[position] = currentPlayer;
 };
 
-const checkWinner = function (gridPosition, currentPlayer) {
-  if (gridPosition === 0) {
-    // 0 - 1 - 2
-    // 0 - 3 - 6
-    // 0 - 4 - 8
-  } else if (gridPosition === 1) {
-    // 1 - 4 - 7
-  } else if (gridPosition === 2) {
-    // 2 - 5 - 8
-    // 2 - 4 - 6
-  } else if (gridPosition === 3) {
-    // 3 - 4 - 5
-  } else if (gridPosition === 6) {
-    // 6 - 7 - 8
-  }
+const showWinner = function (currentPlayer) {
+  modal.classList.add("show-modal");
+  const html = `
+    <div class="modal__text">
+      Player ${currentPlayer} wins!
+    </div>
+  `;
+  modalContent.insertAdjacentHTML("afterbegin", html);
+};
+
+const checkWinner = function (curPlayer) {
+  if (
+    (gridArray[0] === curPlayer &&
+      gridArray[1] === curPlayer &&
+      gridArray[2] === curPlayer) ||
+    (gridArray[0] === curPlayer &&
+      gridArray[3] === curPlayer &&
+      gridArray[6] === curPlayer) ||
+    (gridArray[0] === curPlayer &&
+      gridArray[4] === curPlayer &&
+      gridArray[8] === curPlayer) ||
+    (gridArray[1] === curPlayer &&
+      gridArray[4] === curPlayer &&
+      gridArray[7] === curPlayer) ||
+    (gridArray[2] === curPlayer &&
+      gridArray[4] === curPlayer &&
+      gridArray[6] === curPlayer) ||
+    (gridArray[2] === curPlayer &&
+      gridArray[5] === curPlayer &&
+      gridArray[8] === curPlayer) ||
+    (gridArray[3] === curPlayer &&
+      gridArray[4] === curPlayer &&
+      gridArray[5] === curPlayer) ||
+    (gridArray[6] === curPlayer &&
+      gridArray[7] === curPlayer &&
+      gridArray[8] === curPlayer)
+  )
+    showWinner(curPlayer);
+};
+
+const checkRound = function (b, pos, pl) {
+  assingPlayer(b, pos, pl);
+  checkWinner(pl);
+  changePlayer(pl);
 };
 
 gridButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
     const gridCell = e.target.closest(".grid__cell");
-    const gridPosition = gridCell.dataset.position;
+    const gridPosition = +gridCell.dataset.position;
 
     if (gridArray[gridPosition] === 1 || gridArray[gridPosition] === 2) return;
 
     if (gridArray[gridPosition] === 0) {
       if (player === 1) {
-        assingPlayer(button, player);
+        checkRound(button, gridPosition, player);
       } else if (player === 2) {
-        assingPlayer(button, player);
+        checkRound(button, gridPosition, player);
       }
     }
-    console.log(gridPosition);
   });
 });
