d5b6dc4f9b9fc04a7067d2c9d0a26e2d8ff4b0fe
alanfedrizzi <alanfedrizzi@gmail.com>
2023-02-10 10:42:11 -0300

    light mode button without functionality

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 
 const body = document.querySelector("body");
 const playerSelected = document.querySelector(".header__player-selected");
-const buttonReset = document.querySelector(".header__reset");
+const buttonReset = document.querySelector(".header__button--reset");
 const gridCells = document.querySelectorAll(".grid__cell");
 const gridButtons = document.querySelectorAll(".grid__button");
 const modal = document.querySelector(".modal");
